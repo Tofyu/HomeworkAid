270443d9639541573bd8d781c72cf8b1c3a0de1a
youapotato <74153243+youapotato@users.noreply.github.com>
2023-11-05 15:55:56 -0800

    Update WelcomeScreen.js

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,29 +1,59 @@
 import { StyleSheet, Text, View, FlatList, ImageBackground, TouchableOpacity } from 'react-native'
+import { Button } from '@rneui/themed'
 import React, { useState, useEffect } from 'react'
-import { db } from '../firebase'
+import { db, auth } from '../firebase'
 
-const WelcomeScreen = () => {
+const WelcomeScreen = ({navigation}) => {
     const colours = ['#d1f8ff', '#27d5f5',]
     const [homeworks, setHomeworks] = useState([])
     const [time, setTime] = useState(0)
     const [dueSoon, setDueSoon] = useState([])
+    const [userInfo, setUserInfo] = useState(null)
+    const user = auth.currentUser;
 
     useEffect(() => {
-        let homeworksFromDB = []
-        let dueSoonFromDB = []
-        db.collection('users').doc('k4UBkks0q2pL5RtjZstY').collection('homeworks').onSnapshot((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                console.log(doc.data())
-                homeworksFromDB.push({ id: doc.id, ...doc.data() })
-                console.log("**************")
-                console.log(doc.data().dueDate.toDate())
-                console.log(new Date(Date.now() + 2 * 24 * 60 * 60 * 1000))
-                console.log("********************" + (doc.data().dueDate.toDate() < new Date(Date.now() + 2 * 24 * 60 * 60 * 1000)))
+        const readHomeworks = () => {
+            let homeworksFromDB = []
+            let dueSoonFromDB = []
+            db.collection('users').doc(user.uid).collection('homeworks').onSnapshot((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    console.log(doc.data())
+                    homeworksFromDB.push({ id: doc.id, ...doc.data() })
+                    console.log("**************")
+                    console.log(doc.data().dueDate.toDate())
+                    console.log(new Date(Date.now() + 2 * 24 * 60 * 60 * 1000))
+                    console.log("********************" + (doc.data().dueDate.toDate() < new Date(Date.now() + 2 * 24 * 60 * 60 * 1000)))
+                })
+                console.log("aaaaaaaaaaa", homeworksFromDB.length)
+                setHomeworks([...homeworksFromDB])
+                console.log("homeworks" + homeworks)
             })
-            console.log("aaaaaaaaaaa", homeworksFromDB.length)
-            setHomeworks([...homeworksFromDB])
-            console.log("homeworks" + homeworks)
-        })
+        }
+        
+        const readUser = () => {
+            // Assuming that 'user' is an authenticated user object with a 'uid' property
+            // Get a reference to the document in the 'users' collection using the user's UID
+            const userDocRef = db.collection('users').doc(user.uid);
+          
+            // Use the 'get' method to retrieve the document data
+            userDocRef.get()
+              .then((doc) => {
+                if (doc.exists) {
+                  // Document exists, you can access its data using doc.data()
+                  const userData = doc.data();
+                  setUserInfo(userData)
+                  console.log("User data:", userData);
+                } else {
+                  console.log("No such document!");
+                }
+              })
+              .catch((error) => {
+                console.error("Error getting user document:", error);
+              });
+        };
+        
+        readHomeworks();
+        readUser();
     }, [])
 
     useEffect(() => {
@@ -31,7 +61,7 @@ const WelcomeScreen = () => {
         console.log("*****", homeworks.length)
         setDueSoon(homeworks.filter((item) => {
             return (
-                item.dueDate.toDate() < new Date(Date.now() + 2 * 24 * 60 * 60 * 1000)
+                item.dueDate.toDate() > new Date(Date.now() + 2 * 24 * 60 * 60 * 1000)
             )
         }))
         console.log(dueSoon)
@@ -39,8 +69,8 @@ const WelcomeScreen = () => {
 
     useEffect(() => {
         let sum = 0
-        homeworks.forEach((item) => {
-            sum += item.timeNeeded;
+        dueSoon.forEach((item) => {
+            sum += parseInt(item.timeNeeded);
         })
         console.log(sum)
         setTime(sum)
@@ -62,12 +92,12 @@ const WelcomeScreen = () => {
         <ImageBackground source={require('../assets/background_bottom.png')} resizeMode="cover" style={{ flex: 1, width: '100%', height: '100%' }}>
             <View style={styles.container}>
                 <View style={styles.header}>
-                    <Text style={styles.welcomeText}>Welcome, [name] {new Date().toLocaleDateString()}</Text>
+                    <Text style={styles.welcomeText}>Welcome, {userInfo?.name} </Text>
                 </View>
 
                 <View style={styles.statsContainer}>
                     <View style={styles.statItem}>
-                        <Text style={styles.statLabel}>Total homework time</Text>
+                        <Text style={styles.statLabel}>Total homework minutes</Text>
                         <Text style={styles.statValueCircle}>{time}</Text>
                     </View>
 
@@ -78,13 +108,33 @@ const WelcomeScreen = () => {
                 </View>
 
                 <View style={styles.dueSoonContainer}>
-                    <Text style={styles.dueSoonTitle}>Due Soon:</Text>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    {dueSoon.length > 0 ? (
+                        <Text style={styles.dueSoonTitle}>Due Soon:</Text>
+                    ) : (<Text style={{ fontSize: 16 }}>No homework due soon.</Text>
+                    )}
+                    <Button
+                        title="Add Homework"
+                        buttonStyle={{ backgroundColor: 'rgba(39, 213, 245, 0.8)', borderRadius: 15 }}
+                        titleStyle={{ fontWeight: 'bold', fontSize: 15 }}
+                        icon={{ name: 'pencil-square', type: 'font-awesome', size: 15, color: 'white' }}
+                        onPress={() => { navigation.navigate("Add Homework") }}
+                        style={{ padding: 10, marginVertical: 5, width: 200 }}
+                    />
+                    </View>
                     <FlatList data={dueSoon} renderItem={renderItem} keyExtractor={(item) => item.id} />
                 </View>
 
-                <TouchableOpacity style = {{backgroundColor:'#d1f8ff'}}>
-                    <Text style = {styles.text1}>To Home Screen</Text>
-                </TouchableOpacity>
+                <View style={{alignItems:'center'}}>
+                    <Button
+                        title="Move to Home"
+                        buttonStyle={{ backgroundColor: 'rgba(39, 213, 245, 0.8)', borderRadius: 15 }}
+                        titleStyle={{ fontWeight: 'bold', fontSize: 15 }}
+                        icon={{ name: 'home', type: 'font-awesome', size: 15, color: 'white' }}
+                        onPress={() => navigation.navigate('Home')}
+                        style={{ padding: 10, marginVertical: 5, width: 200 }}
+                    />
+                </View>
             </View>
         </ImageBackground>
     )
@@ -165,7 +215,7 @@ const styles = StyleSheet.create({
     },
     statValueCircle: {
         backgroundColor: '#27D5FAD3',
-        borderRadius: 50,
+        borderRadius: 10,
         padding: 10,
         color: 'white',
         fontWeight: 'bold',
